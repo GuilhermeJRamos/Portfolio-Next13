a2b5134f0c2ad6a9a4adc0cd978bd05f28b906b2
GuilhermeJRamos <guilhermedejesusramos23@gmail.com>
2023-09-10 17:44:39 -0300

    ajustes

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,13 +4,12 @@ export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; 2030 Ricardo. All rights reserved.
+        &copy; 2023 Guilherme. Todos os direitos Reservados.
       </small>
       <p className="text-xs">
-        <span className="font-semibold">About this website:</span> built with
-        React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
-        Framer Motion, React Email & Resend, Vercel hosting.
-      </p>
+  <span className="font-semibold">Sobre este site:</span> construído com React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS, Framer Motion, React Email & Resend, hospedado na Vercel.
+</p>
+
     </footer>
   );
 }
